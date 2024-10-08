ab90ce2747d00a6cb138a7d9b78c1908ad20cb46
Kenny Doan <kdoan93@gmail.com>
2024-10-08 19:32:31 -0400

    WIP adding user input field

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
+import UserInput from "@/components/UserInput";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+
+    
+
     return (
         <View style={styles.container}>
-            <Text>App.js page</Text>
+            <Text>Sleeper fantasy data</Text>
+            <Text>{
+            `Input your username here to receive your
+                Sleeper fantasy team data`
+            }</Text>
+
+            <UserInput/>
+
             <StatusBar style="auto" />
         </View>
     )
---
+++ components/UserInput.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { View, TextInput, Text, StyleSheet } from "react-native";
+
+const UserInput = () => {
+    const [user, setUser] = useState('')
+    const [userId, setUserId] = useState('')
+
+    async function fetchUser(user: string) {
+        try {
+            const response = await fetch(`https://api.sleeper.app/v1/user/${user}`)
+            const data = await response.json()
+            setUser(data.display_name)
+            setUserId(data.user_id)
+
+            console.log(data)
+
+        } catch (error) {
+            console.error("Error fetching user data: ", error)
+        }
+    }
+
+    // fetchUser('Jumbo_Wumbo')
+
+    console.log("user: ", user)
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter your username here"
+                value={user}
+                onChangeText={newUser => fetchUser(newUser)}
+            />
+            <Text style={styles.text}>
+                {`
+                    User: ${user}
+                    User ID: ${userId}
+                `}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingHorizontal: 10,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 18,
+    }
+})
+
+export default UserInput;
